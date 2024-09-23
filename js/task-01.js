5589a7a96149f05252056d9cd2e75d5513f41cf0
xDagomex <adaskubiak@wp.pl>
2024-09-23 21:30:05 +0200

    10

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,7 @@
 const listWithID = document.querySelectorAll('#categories .item');
 console.log("Number of categories: " + listWithID.length);
 
+listWithID.forEach(ID => {
+    const listName = ID.querySelector('h2').textContent
+    console.log(listName)
+})
