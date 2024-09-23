2cc79940894ba1e69f07d1bc2d863c01195edf7c
xDagomex <adaskubiak@wp.pl>
2024-09-23 22:22:09 +0200

    12

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+const listItems = [];
+
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  listItems.push(listItem);
+})
+
+listIngredients.append(...listItems)
