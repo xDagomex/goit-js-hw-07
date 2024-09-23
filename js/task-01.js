f223776623827be48e165653efc122cd25fb810e
xDagomex <adaskubiak@wp.pl>
2024-09-23 20:53:01 +0200

    8

--- js/task-01.js
+++ js/task-01.js
@@ -2,4 +2,3 @@ const listWithID = document.querySelector('#categories');
 const menuItemsByTagName = listWithID.querySelectorAll(".item");
 console.log("Number of categories: " + menuItemsByTagName.length);
 
-console.log(menuItemsByTagName);
