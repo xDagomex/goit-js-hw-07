9098ff75761209ec43fcb332a15fa587b001ed77
xDagomex <adaskubiak@wp.pl>
2024-09-23 04:47:03 +0200

    2

---
+++ task-01.js
@@ -0,0 +1,13 @@
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = body.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
